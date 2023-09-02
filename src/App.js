a4e5d901e0f0e6dfeb9ae715d205191072ded4d7
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-09-02 13:05:50 +0500

    React_Dynamic_Button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Buttton";
+import "./style.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Dynamic Buttons</h1>
+      <Button name={"Blue"} className={"blue"} />
+      <Button name={"Green"} className={"green"} />
+      <Button name={"Orange"} className={"orange"} />
+      <Button name={"Red"} className={"red"} />
+      <Button name={"Outlined"} className={"outlined-blue"} />
     </div>
   );
 }
